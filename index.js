fcee9916e120875285e6cbcb68f21fb704382057
Evan Miller <evandelaymiller12@gmail.com>
2019-11-07 16:21:59 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+$(function() {
+console.log("K");
+
+$(function addItem() {
+
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        
+        let userInput = $(event.currentTarget).find('input').val();
+        
+        let newitem = `<li>
+        <span class="shopping-item">${userInput}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`
+
+        $('ul').append(newitem);
+       
+
+});
+});
+
+
+$(function deleteItem(){
+
+    $('.shopping-item-delete').on('click', function(event){
+        $( event.target ).closest( "li" ).remove();
+
+    });
+
+});
+
+$(function checkItem(){
+
+   $('.shopping-item-toggle').on('click', function(event){
+  $(event.currentTarget).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    //$('span.shopping-item').toggleClass();
+
+});
+});
+
+});
